8153fd9b16c6e3f90f261c88a3676cd733256137
omgitsmoe <60219950+omgitsmoe@users.noreply.github.com>
2021-03-26 00:35:21 +0100

    fixed dfs ordering when queueing child nodes

--- utils.zig
+++ utils.zig
@@ -4,23 +4,22 @@ const std = @import("std");
 pub fn DepthFirstIterator(comptime T: type) type {
     return struct {
         const Self = @This();  // polymorphic type
-        const Queue = std.TailQueue(*T);
+        const Queue = std.TailQueue(T);
         const QNode = Queue.Node;  // node type used in TailQueue
 
-        allocator: *std.mem.Allocator,
-        node_buf: std.ArrayList(QNode),
+        // can't use an ArrayList with this since the ptrs get invalidated on resize
+        node_buf: std.heap.ArenaAllocator,
         q: Queue,
 
-        pub fn init(allocator: *std.mem.Allocator, start: *T) !Self {
+        pub fn init(allocator: *std.mem.Allocator, start: T) !Self {
             var dfs = Self{
-                .allocator = allocator,
-                .node_buf = std.ArrayList(QNode).init(allocator),
+                .node_buf = std.heap.ArenaAllocator.init(allocator),
                 .q = Queue{},
             };
 
-            const added = try dfs.node_buf.addOne();
+            const added = try dfs.node_buf.allocator.create(QNode);
             added.* = QNode{ .data = start };
-            dfs.q.append(added);
+            dfs.q.prepend(added);
 
             return dfs;
         }
@@ -29,17 +28,20 @@ pub fn DepthFirstIterator(comptime T: type) type {
             self.node_buf.deinit();
         }
 
-        pub fn next(self: *Self) ?*T {
+        pub fn next(self: *Self) !?T {
             if (self.q.len > 0) {
-                const current_item = self.q.popFirst();
+                // we can use .? (short for "orelse unreachable") since we checked that q has items
+                const current_item = self.q.popFirst().?;
 
                 // queue the children
                 if (current_item.data.first_child) |first_child| {
                     var current_child = first_child;
-                    self.queue_left(QNode{ .data = first_child });
+                    // in order to preserve the child order we have to use TailQueue.insertAfter
+                    // the last queued child for every child beyond the first
+                    var last_queued = try self.queue_left(QNode{ .data = first_child });
 
                     while (current_child.next) |child| {
-                        self.queue_left(QNode{ .data = child });
+                        last_queued = try self.queue_after(last_queued, QNode{ .data = child });
                         current_child = child;
                     }
                 }
@@ -50,16 +52,26 @@ pub fn DepthFirstIterator(comptime T: type) type {
             }
         }
 
-        fn queue(self: *Self, node: QNode) !void {
-            const new = try self.q.addOne();
+        fn queue(self: *Self, node: QNode) !*QNode {
+            // TODO look up in stdlib how ArenaAllocator is used (storing allocator separate?)
+            const new = try self.node_buf.allocator.create(QNode);
             new.* = node;
             self.q.append(new);
+            return new;
         }
 
-        fn queue_left(self: *Self, node: QNode) !void {
-            const new = try self.q.addOne();
+        fn queue_after(self: *Self, after: *QNode, node: QNode) !*QNode {
+            const new = try self.node_buf.allocator.create(QNode);
+            new.* = node;
+            self.q.insertAfter(after, new);
+            return new;
+        }
+
+        fn queue_left(self: *Self, node: QNode) !*QNode {
+            const new = try self.node_buf.allocator.create(QNode);
             new.* = node;
             self.q.prepend(new);
+            return new;
         }
     };
 }
