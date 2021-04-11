5ea4f33a80f48942a2321d178814b8f6979a3c1b
omgitsmoe <60219950+omgitsmoe@users.noreply.github.com>
2021-04-11 17:18:55 +0200

    fix: actually output length in bytes

--- lang_helpers/python.py
+++ lang_helpers/python.py
@@ -11,8 +11,13 @@ class BufferedIOHelper:
         self.buffer = []
 
     def write(self, s):
+        # since we use stdout in binary mode anyway encode the string
+        # (need to anyway to get the length in bytes)
+        char_len = len(s)
+        s = s.encode('utf-8')
         self.buffer.append(s)
-        return len(s)
+        # don't know if this must return length in chars?
+        return char_len
 
     def flush(self):
         # nop
@@ -24,11 +29,12 @@ class BufferedIOHelper:
         # so we can write the length as unsigned long
         # closefd=False so file descr doesn't get close by ctx manager
         with os.fdopen(self.text_io.fileno(), "wb", closefd=False) as stdio:
-            stdio.write(struct.pack('L', len(self.buffer)))
+            stdio.write(struct.pack('L', sum(len(s) for s in self.buffer)))
             stdio.flush()
 
-        self.text_io.write("".join(self.buffer))
-        self.text_io.flush()
+            for binary_string in self.buffer:
+                stdio.write(binary_string)
+            stdio.flush()
 
 
 # replace out/err with our version that waits till real_flush is called
